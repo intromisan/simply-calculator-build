81e5284cf2418e4abe43c6041b1cd3b0fbc84716
intromisan <42156539+intromisan@users.noreply.github.com>
2020-08-07 22:08:30 +0200

    Initial Commit

---
+++ precache-manifest.6a508609ffdaadbdb010c11b1b2648d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45794faf66409a041df2a4798374d753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ea9fe36aa3cfa52389c",
+    "url": "/static/css/main.2821377a.chunk.css"
+  },
+  {
+    "revision": "1a016cd7182b0c74b9d0",
+    "url": "/static/js/2.dcbb1cac.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/static/js/2.dcbb1cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea9fe36aa3cfa52389c",
+    "url": "/static/js/main.d163b376.chunk.js"
+  },
+  {
+    "revision": "1cff3457b5b7c00e14d3",
+    "url": "/static/js/runtime-main.1e6afa1b.js"
+  }
+]);
